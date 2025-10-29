0eaf78a4f0a1f768ca2ea1159b7c77c48026d5ab
Roi <rohithramesh1991@gmail.com>
2025-10-29 13:21:05 +0000

    prometheus metrics 10

--- webapp/main.py
+++ webapp/main.py
@@ -45,7 +45,7 @@ def root():
 # -----------------------------------------------------------------------------
 # Prometheus /metrics endpoint
 # -----------------------------------------------------------------------------
-Instrumentator().instrument(app).expose(app, endpoint="/metrics", include_in_schema=True)
+Instrumentator().instrument(app).expose(app)
 
 # -----------------------------------------------------------------------------
 # Entry point for Uvicorn / Gunicorn
